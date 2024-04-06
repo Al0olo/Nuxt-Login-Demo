7d7b4c461fc76ccb98d5102e217f6dd580635791
ali <straoid@gmail.com>
2024-04-06 23:11:11 +0200

    first commit

---
+++ pages/hello.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div class="flex justify-center pt-6">
+      <span><b>Login Success</b></span>
+    </div>
+    <div class="flex justify-center pt-6">
+      <v-btn text class="w-60 bg-blue-600 hover:bg-blue-900 text-white" @click="logOut">
+        Logout
+      </v-btn>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent, useContext, useRouter } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  middleware: 'auth',
+  setup () {
+    const { $auth }: any = useContext()
+    const router = useRouter()
+
+    const logOut = async () => {
+      await $auth.logout()
+      await router.push('/login')
+    }
+    return { logOut }
+  }
+})
+</script>
